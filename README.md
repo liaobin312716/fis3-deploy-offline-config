dc6150540366d25145d97c112933ea79724a5ebd
liaobin_iwm <liaobin@iwaimai.baidu.com>
2016-08-24 20:59:07 +0800

    add README.md

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # fis3-packager-offline-config
-离线组件配置生成器
+自动生成离线组件配置工具
+
+##配置方式
+目前只支持一下调用方式：
+
+```javascript
+
+fis.match('::package', {
+  packager:fis.plugin('offline-config',{
+    'id':'bdwm.plugin.ribao',
+ 	'reg':'page/**',
+    'output':'config.json'
+  }
+})
+
+```
+配置参数：
+	
+>id:    [必须]  离线组件id
+
+>reg:   [必须] 匹配哪些文件，类似fis.match()的参数;*目前不支持排除某个文件*
+
+>output: [必须] 配置文件名称(如：config.json)
+
+
+##运行结果
+
+生成config.json文件，文件名可配置
+
+```javascript
+{
+  "id":"bdwm.plugin.banff",
+  "pages":[
+	 {
+	   "name":"b",
+	   "file":"/views/a/b.html"
+	 },
+	 {
+	   "name":"index",
+		"file":"/views/index.html"
+	 }
+   ]
+}
+
+```
+  
+file 会根据 release 配置生成编译后的路径。
--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-console.log('ssss');
+/**
+ * 根据页面自动生成离线config.json
+ * 用法：
+ * fis.match('::package', {
+ *   packager:fis.plugin('offline-config',{
+ *		'id':'bdwm.plugin.ribao',
+ 		'reg':'page/**', // or ['page/a/**','page/b/**']
+ *      'output':'config.json'
+ *   }
+ * })
+ *
+ **/
+
+
+var path = require('path');
+var _ = fis.util;
+
+module.exports = function (ret, conf, settings, opt) {
+    
+    var root = fis.project.getProjectPath();
+
+    if (!settings || !Object.keys(settings).length) {
+	  throw new Error('TypeError: only Object are allowed.');
+	}
+	if(!settings.id){
+		throw new Error('PropertyError: property id can not be empty.');
+	}
+	if(!settings.reg){
+		throw new Error('PropertyError: property reg can not be empty.');
+	}
+
+	// 还缺乏参数严格校验
+
+	var src = ret.src;
+	var output = settings.output || 'config.json';
+	var sources = [];
+	var config = { // config 模板
+		id:settings.id,
+		pages:[]
+	}
+
+    // 源文件生成数组
+    Object.keys(src).forEach(function (key) {
+      sources.push(src[key]);
+    });
+
+	var patterns = settings.reg;
+    if (!Array.isArray(patterns)) {
+      patterns = [patterns];
+    }
+
+    var valid = patterns.every(function(pattern) {
+        console.log(pattern)
+      return typeof pattern === 'string' || pattern instanceof RegExp;
+    });
+
+    if (!valid) {
+      throw new Error('TypeError: only string and RegExp are allowed.');
+    }
+
+
+    var list = [];
+
+    patterns.forEach(function (pattern, index) {
+        var reg = pattern;
+        if (typeof reg === 'string') {
+          reg = _.glob(reg);
+        }
+    	var mathes = src[reg] || sources.filter(function (file) {
+        reg.lastIndex = 0;
+        return (reg.test(file.subpath));
+      });
+    	list = mathes;
+    });
+
+    list.forEach(function (item, index) {
+    	config.pages.push({
+    		name:item.filename,
+    		file:item.getUrl()
+    	})
+    });
+
+	 var pkg = fis.file.wrap(path.join(root, output));
+     pkg.setContent(JSON.stringify(config));
+	 ret.pkg[output] = pkg; 
+}
