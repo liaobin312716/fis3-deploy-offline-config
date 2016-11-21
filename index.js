3b486bda2aab2ec08be4590e83d1053128ec4cc9
liaobin_iwm <liaobin@iwaimai.baidu.com>
2016-11-21 17:59:42 +0800

    update

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  * fis.match('::package', {
  *   packager:fis.plugin('offline-config',{
  *		'id':'bdwm.plugin.ribao',
- 		'reg':'page/**', // or ['page/a/**','page/b/**']
- *      'output':'config.json'
+ *		'reg':'page/**', // or ['page/a/**','page/b/**']
+ *    'output':'config.json'
  *   }
  * })
  *
@@ -16,70 +16,76 @@ var path = require('path');
 var _ = fis.util;
 
 module.exports = function (ret, conf, settings, opt) {
-    
-    var root = fis.project.getProjectPath();
 
-    if (!settings || !Object.keys(settings).length) {
-	  throw new Error('TypeError: only Object are allowed.');
-	}
-	if(!settings.id){
-		throw new Error('PropertyError: property id can not be empty.');
-	}
-	if(!settings.reg){
-		throw new Error('PropertyError: property reg can not be empty.');
-	}
+  var root = fis.project.getProjectPath();
 
-	// 还缺乏参数严格校验
+  if (!settings || !Object.keys(settings).length) {
+    throw new Error('TypeError: only Object are allowed.');
+  }
+  if (!settings.id) {
+    throw new Error('PropertyError: property id can not be empty.');
+  }
+  if (!settings.reg) {
+    throw new Error('PropertyError: property reg can not be empty.');
+  }
 
-	var src = ret.src;
-	var output = settings.output || 'config.json';
-	var sources = [];
-	var config = { // config 模板
-		id:settings.id,
-		pages:[]
-	}
+  // 还缺乏参数严格校验
 
-    // 源文件生成数组
-    Object.keys(src).forEach(function (key) {
-      sources.push(src[key]);
-    });
+  var src = ret.src;
+  var output = settings.output || 'config.json';
+  var sources = [];
+  var config = { // config 模板
+    id   : settings.id,
+    pages: []
+  }
 
-	var patterns = settings.reg;
-    if (!Array.isArray(patterns)) {
-      patterns = [patterns];
+  // 源文件生成数组
+  Object.keys(src).forEach(function (key) {
+    sources.push(src[key]);
+  });
+
+  var patterns = settings.reg;
+  if (!Array.isArray(patterns)) {
+    patterns = [patterns];
+  }
+
+  var valid = patterns.every(function (pattern) {
+    return typeof pattern === 'string' || pattern instanceof RegExp;
+  });
+
+  if (!valid) {
+    throw new Error('TypeError: only string and RegExp are allowed.');
+  }
+
+  var list = [];
+
+  patterns.forEach(function (pattern, index) {
+    var reg = pattern;
+    if (typeof reg === 'string') {
+      reg = _.glob(reg);
     }
-
-    var valid = patterns.every(function(pattern) {
-      return typeof pattern === 'string' || pattern instanceof RegExp;
-    });
-
-    if (!valid) {
-      throw new Error('TypeError: only string and RegExp are allowed.');
-    }
-
-
-    var list = [];
-
-    patterns.forEach(function (pattern, index) {
-        var reg = pattern;
-        if (typeof reg === 'string') {
-          reg = _.glob(reg);
-        }
-    	var mathes = src[reg] || sources.filter(function (file) {
+    var mathes = src[reg] || sources.filter(function (file) {
         reg.lastIndex = 0;
         return (reg.test(file.subpath));
       });
-    	list = mathes;
-    });
+    list = mathes;
+  });
 
-    list.forEach(function (item, index) {
-    	config.pages.push({
-    		name:item.filename,
-    		file:item.getUrl()
-    	})
-    });
+  list.forEach(function (item, index) {
+    config.pages.push({
+      name: item.filename,
+      file: item.getUrl()
+    })
+  });
 
-	 var pkg = fis.file.wrap(path.join(root, output));
-     pkg.setContent(JSON.stringify(config));
-	 ret.pkg[output] = pkg; 
+  var pkg = fis.file.wrap(path.join(root, output));
+  if (/\.json$/.test(output)) {
+    pkg.setContent(JSON.stringify(config));
+  } else {
+    pkg.setContent(jsonWrapper(JSON.stringify(config));
+  }
+  ret.pkg[output] = pkg;
+  function jsonWrapper(jsonStr) {
+    return "define('" + output + "', function(require, exports, module) { module.exports = " + jsonStr + ';})';
+  }
 }
